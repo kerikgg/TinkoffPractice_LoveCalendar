eb2b743cfbab1dd14a53e59afd44403f4ebd3c8c
kerikgg <kirill.abramov.24@mail.ru>
2024-03-27 00:15:23 +0300

    add project

---
+++ LoveCalendar/LoveCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoveCalendar
+//
+//  Created by kerik on 26.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
