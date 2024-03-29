e9a85d46c12309b9091dd99b70443254af374ecf
kerikgg <kirill.abramov.24@mail.ru>
2024-03-30 00:17:54 +0300

    add auth screen

--- LoveCalendar/LoveCalendar/Flows/Auth/Screen/AuthViewController.swift
+++ LoveCalendar/LoveCalendar/Flows/Auth/Screen/AuthViewController.swift
@@ -6,11 +6,72 @@
 //
 
 import UIKit
+import SnapKit
 
-class AuthViewController: UIViewController {
+enum AuthStates {
+    case signIn
+    case signUp
+}
+
+class AuthViewController: UIViewController, FlowControllerWithValue {
+    private lazy var logoImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    private lazy var signInButton: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.layer.cornerRadius = 8
+        button.setTitle("Войти", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(named: "ButtonTextColor")
+        return button
+    }()
+
+    private lazy var signUpButton: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.layer.cornerRadius = 8
+        button.setTitle("Зарегестрироваться", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(named: "ButtonTextColor")
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setLayout()
+    }
+
+    var completionHandler: ((AuthStates) -> Void)?
+}
+
+extension AuthViewController {
+    private func setLayout() {
+        let stackView = UIStackView(arrangedSubviews: [signInButton, signUpButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+
+        addSubviews(logoImage, stackView)
+
+        logoImage.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(250)
+            make.height.equalTo(150)
+            make.top.equalToSuperview().offset(view.frame.height * 0.3)
+        }
+
+        stackView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(200)
+            make.top.equalTo(logoImage).offset(180)
+        }
+    }
+}
+
+extension AuthViewController {
+    func addSubviews(_ subviews: UIView...) {
+        subviews.forEach { view.addSubview($0) }
     }
 }
